91239fd5482854704e25da27b85744b88b13de9a
User <user@MacBook-Air-3.local>
2025-02-22 19:12:07 +0300

    lab 2

---
+++ my-rent-service/src/components/app/app.tsx
@@ -0,0 +1,8 @@
+import { JSX } from "react";
+import MainPage from "../../pagees/main-page/main-page";
+function app(): JSX.Element {
+    return(
+        <MainPage />
+          );
+}
+export default app;
